28daf6f3f330c511aec7a86035934fb6cce81d08
Ali <101870797+alitarakci2@users.noreply.github.com>
2023-04-28 17:01:07 +0300

    inserting data

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (0, 'Frodo Baggins', 'self-employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (65900, 'Abraham Lincoln', 'unemployed');
